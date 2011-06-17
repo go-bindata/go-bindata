c539ce1afc84856f71d6fc1932a86c66fe5a37dd
jim teeuwen <jimteeuwen@gmail.com>
2011-06-17 18:01:49 +0200

    wrong license header in main.go

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
-// Copyright (c) 2010, Jim Teeuwen. All rights reserved.
-// This code is subject to a 1-clause BSD license.
-// See the LICENSE file for its contents.
+// This work is subject to the CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+// license. Its contents can be found at:
+// http://creativecommons.org/publicdomain/zero/1.0/
 
 package main
 
