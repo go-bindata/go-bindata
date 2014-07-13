c15090e68a45dc7b298c5d2a14173f4bc22afe1a
Elazar Leibovich <elazarl@gmail.com>
2014-07-13 09:59:24 +0300

    Support directory listing with AssetDir(dirname string)
    Signed-off-by: Elazar Leibovich <elazarl@gmail.com>

--- toc.go
+++ toc.go
@@ -7,7 +7,120 @@ package bindata
 import (
 	"fmt"
 	"io"
+	"os"
+	"strings"
 )
+
+type assetTree struct {
+	Asset    Asset
+	Children map[string]*assetTree
+}
+
+func newAssetTree() *assetTree {
+	tree := &assetTree{}
+	tree.Children = make(map[string]*assetTree)
+	return tree
+}
+
+func (node *assetTree) child(name string) *assetTree {
+	rv, ok := node.Children[name]
+	if !ok {
+		rv = newAssetTree()
+		node.Children[name] = rv
+	}
+	return rv
+}
+
+func (root *assetTree) Add(route []string, asset Asset) {
+	for _, name := range route {
+		root = root.child(name)
+	}
+	root.Asset = asset
+}
+
+func ident(w io.Writer, n int) {
+	for i := 0; i < n; i++ {
+		w.Write([]byte{'\t'})
+	}
+}
+
+func (root *assetTree) funcOrNil() string {
+	if root.Asset.Func == "" {
+		return "nil"
+	} else {
+		return root.Asset.Func
+	}
+}
+
+func (root *assetTree) writeGoMap(w io.Writer, nident int) {
+	fmt.Fprintf(w, "&_bintree_t{%s, map[string]*_bintree_t{\n", root.funcOrNil())
+	for p, child := range root.Children {
+		ident(w, nident+1)
+		fmt.Fprintf(w, `"%s": `, p)
+		child.writeGoMap(w, nident+1)
+	}
+	ident(w, nident)
+	io.WriteString(w, "}}")
+	if nident > 0 {
+		io.WriteString(w, ",")
+	}
+	io.WriteString(w, "\n")
+}
+
+func (root *assetTree) WriteAsGoMap(w io.Writer) error {
+	_, err := fmt.Fprint(w, `type _bintree_t struct {
+	Func func() ([]byte, error)
+	Children map[string]*_bintree_t
+}
+var _bintree = `)
+	root.writeGoMap(w, 0)
+	return err
+}
+
+func writeTOCTree(w io.Writer, toc []Asset) error {
+	_, err := fmt.Fprintf(w, `// AssetDir returns the file names below a certain
+// directory embedded in the file by go-bindata.
+// For example if you run go-bindata on data/... and data contains the
+// following hierarchy:
+//     data/
+//       foo.txt
+//       img/
+//         a.png
+//         b.png
+// then AssetDir("data") would return []string{"foo.txt", "img"}
+// AssetDir("data/img") would return []string{"a.png", "b.png"}
+// AssetDir("foo.txt") and AssetDir("notexist") would return an error
+func AssetDir(name string) ([]string, error) {
+	cannonicalName := strings.Replace(name, "\\", "/", -1)
+	pathList := strings.Split(cannonicalName, "/")
+	node := _bintree
+	for _, p := range pathList {
+		node = node.Children[p]
+		if node == nil {
+			return nil, fmt.Errorf("Asset %%s not found", name)
+		}
+	}
+	if node.Func != nil {
+		return nil, fmt.Errorf("Asset %%s not found", name)
+	}
+	rv := make([]string, 0, len(node.Children))
+	for name := range node.Children {
+		rv = append(rv, name)
+	}
+	return rv, nil
+}
+
+`)
+	if err != nil {
+		return err
+	}
+	tree := newAssetTree()
+	for i := range toc {
+		pathList := strings.Split(toc[i].Name, string(os.PathSeparator))
+		tree.Add(pathList, toc[i])
+	}
+	return tree.WriteAsGoMap(w)
+}
 
 // writeTOC writes the table of contents file.
 func writeTOC(w io.Writer, toc []Asset) error {
