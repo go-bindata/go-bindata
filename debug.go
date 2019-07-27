5bb656e000d66d229698956100a825f1be56e435
christoph-k <16186587+christoph-k@users.noreply.github.com>
2019-07-27 23:32:37 +0200

    fix invalid code with -fs and -debug flag

--- debug.go
+++ debug.go
@@ -72,6 +72,43 @@ type asset struct {
 	info  os.FileInfo
 }
 
+type bindataFileInfo struct {
+	name    string
+	size    int64
+	mode    os.FileMode
+	modTime time.Time
+}
+
+// Name return file name
+func (fi bindataFileInfo) Name() string {
+	return fi.name
+}
+
+// Size return file size
+func (fi bindataFileInfo) Size() int64 {
+	return fi.size
+}
+
+// Mode return file mode
+func (fi bindataFileInfo) Mode() os.FileMode {
+	return fi.mode
+}
+
+// ModTime return file modify time
+func (fi bindataFileInfo) ModTime() time.Time {
+	return fi.modTime
+}
+
+// IsDir return file whether a directory
+func (fi bindataFileInfo) IsDir() bool {
+	return fi.mode&os.ModeDir != 0
+}
+
+// Sys return file is sys mode
+func (fi bindataFileInfo) Sys() interface{} {
+	return nil
+}
+
 `, header)
 	return err
 }
