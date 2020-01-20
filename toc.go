5ff08bbcdae83c963b89d61afbc2ec7ae0178562
Josh Soref <jsoref@users.noreply.github.com>
2020-01-20 14:03:23 -0500

    spelling: nonexistent

--- toc.go
+++ toc.go
@@ -151,7 +151,7 @@ func writeTOCTree(w io.Writer, toc []Asset) error {
 //         b.png
 // then AssetDir("data") would return []string{"foo.txt", "img"}
 // AssetDir("data/img") would return []string{"a.png", "b.png"}
-// AssetDir("foo.txt") and AssetDir("notexist") would return an error
+// AssetDir("foo.txt") and AssetDir("nonexistent") would return an error
 // AssetDir("") will return []string{"data"}.
 func AssetDir(name string) ([]string, error) {
 	node := _bintree
